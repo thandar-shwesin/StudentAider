3f49f7b1f59572c9fdd9fefb25e7bb58df815d50
Thandar <juliashwesin@gmail.com>
2021-01-02 21:35:57 +0630

    StudentAider

---
+++ src/stuAdier/testingPdf.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stuAdier;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.*;
+
+/**
+ *
+ * @author TSS
+ */
+public class testingPdf {
+    
+    
+    
+    public testingPdf(){
+        
+    }
+    
+    myChooser mychooser=new myChooser();
+    
+    
+}
